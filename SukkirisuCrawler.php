ec213becd6a76aa33b9242ed3652617276f25c1b
f8hash <windboy.fhash@gmail.com>
2020-05-23 23:59:05 +0900

    Update クラスセレクタから超スッキりす〜ガッカりすを判定する
    テストが落ちる

--- SukkirisuCrawler.php
+++ SukkirisuCrawler.php
@@ -22,6 +22,31 @@ class SukkirisuCrawler implements CrawlerInterface
     public function get(): array
     {
         $crawler = $this->client->request('GET', $this->url);
-        return explode(' ', $crawler->filter('#month'.$this->month)->text());
+
+        // 超スッキりすからガッカりすまでで4段階ある
+        $types = [
+            'type1' => '超スッキりす',
+            'type2' => 'スッキりす',
+            'type3' => 'まあまあスッキりす',
+            'type4' => 'ガッカりす',
+        ];
+
+        $ret = [];
+        foreach ($types as $key => $type) {
+            $divId = "#main #month{$this->month}.{$key}";
+
+            // TODO テスト実行時にここを通過せずにUndefined index: textになる
+            if (count($crawler->filter($divId))) {
+                $ret['type'] = $type;
+                $ret['text'] = explode(' ', $crawler->filter($divId)->text());
+            }
+        }
+
+        // TODO スクレイピング先に変更があった場合、想定していた位置に要素がなくなる
+        // if (empty($ret)) {
+        //     throw new Exception('ページの任意の要素に結果が存在しない。');
+        // }
+
+        return $ret['text'];
     }
 }
