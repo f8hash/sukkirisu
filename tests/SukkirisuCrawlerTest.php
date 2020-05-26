b2107e1d7b461633cbdd4c5906a4e61ebe75eb0a
f8hash <windboy.fhash@gmail.com>
2020-05-26 23:57:44 +0900

    Update ガッカりすをテストに定義

--- tests/SukkirisuCrawlerTest.php
+++ tests/SukkirisuCrawlerTest.php
@@ -102,4 +102,35 @@ class SukkirisuCrawlerTest extends TestCase
         $this->assertContains('暑くなって体が弱っているので睡眠をしっかり', $result);
         $this->assertContains('茶', $result);
     }
+
+    /**
+     * @test
+     */
+    public function ガッカりす(): void
+    {
+        // メソッドチェーンのモック化
+        // filter()->text()
+        $crawlerStub = $this->createMock(Crawler::class);
+        $crawlerStub->method('text')->willReturn('7 暑くなって体が弱っているので睡眠をしっかり 茶');
+        
+        $crawlerStub->method('filter')->will(
+            $this->onConsecutiveCalls(
+                [], // #month7.type1の要素は存在しない
+                [], // #month7.type2の要素は存在しない
+                [], // #month7.type3の要素は存在しない
+                [[]], // #month7.type4の要素が存在する
+                $this->returnSelf(), // text()を呼び出すために自身を返す
+            )
+        );
+
+        $clientStub = $this->createMock(Client::class);
+        $clientStub->method('request')->willReturn($crawlerStub);
+
+        $sukkirisuCrawler = new Sukkirisu\SukkirisuCrawler($clientStub);
+        $result = $sukkirisuCrawler->get();
+        $this->assertIsArray($result);
+        $this->assertContains('7', $result);
+        $this->assertContains('暑くなって体が弱っているので睡眠をしっかり', $result);
+        $this->assertContains('茶', $result);
+    }
 }
