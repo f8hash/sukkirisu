268aba9aadad222bededd56f063c6a7363b250f2
f8hash <windboy.fhash@gmail.com>
2020-05-28 00:39:14 +0900

    Add 各結果の表示テストパターンを追加

--- tests/ConsoleViewerTest.php
+++ tests/ConsoleViewerTest.php
@@ -25,4 +25,61 @@ class ConsoleViewerTest extends TestCase
         $sukkirisuView = new Sukkirisu\ConsoleViewer($crawlerStub);
         $sukkirisuView->show();
     }
+
+    /**
+     * @test
+     */
+    public function 超スッキりすの結果表示(): void
+    {
+        $this->expectOutputString('7月生まれは1位。ちょっとした気のゆるみで大きなミスを犯すことも。ラッキーカラーは緑');
+
+        $crawlerStub = $this->createMock(Sukkirisu\CrawlerInterface::class);
+        $crawlerStub->method('get')->willReturn([
+            'month' => '7',
+            'rank' => '1位',
+            'result' => 'ちょっとした気のゆるみで大きなミスを犯すことも',
+            'color' => '緑'
+        ]);
+
+        $sukkirisuView = new Sukkirisu\ConsoleViewer($crawlerStub);
+        $sukkirisuView->show();
+    }
+
+    /**
+     * @test
+     */
+    public function スッキりすの結果表示(): void
+    {
+        $this->expectOutputString('7月生まれは5位。ちょっとした気のゆるみで大きなミスを犯すことも。ラッキーカラーは緑');
+
+        $crawlerStub = $this->createMock(Sukkirisu\CrawlerInterface::class);
+        $crawlerStub->method('get')->willReturn([
+            'month' => '7',
+            'rank' => '5位',
+            'result' => 'ちょっとした気のゆるみで大きなミスを犯すことも',
+            'color' => '緑'
+        ]);
+
+        $sukkirisuView = new Sukkirisu\ConsoleViewer($crawlerStub);
+        $sukkirisuView->show();
+    }
+
+    /**
+     * @test
+     */
+    public function ガッカりすの結果表示(): void
+    {
+        $this->expectOutputString('7月生まれは12位。ちょっとした気のゆるみで大きなミスを犯すことも。ラッキーカラーは緑');
+
+        $crawlerStub = $this->createMock(Sukkirisu\CrawlerInterface::class);
+        $crawlerStub->method('get')->willReturn([
+            'month' => '7',
+            'rank' => '12位',
+            'result' => 'ちょっとした気のゆるみで大きなミスを犯すことも',
+            'color' => '緑'
+        ]);
+
+        $sukkirisuView = new Sukkirisu\ConsoleViewer($crawlerStub);
+        $sukkirisuView->show();
+    }
 }
