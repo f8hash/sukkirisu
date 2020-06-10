6dcb05ccde326445db4c95da4faef294d7643405
f8hash <windboy.fhash@gmail.com>
2020-06-10 18:33:05 +0900

    Update サイト情報をサイトクラスから取得
    TODO：誕生月占いクラスを継承したスッキりすクラスを作成する

--- BirthMonthFortuneTeller.php
+++ BirthMonthFortuneTeller.php
@@ -5,6 +5,7 @@ require_once('SiteInterface.php');
 
 use DOMWrap\Document;
 
+// TODO スッキりす用の派生クラスを作成
 class BirthMonthFortuneTeller implements FortuneTellerInterface
 {
     // 以下の要素を読み取るクラス
@@ -16,13 +17,10 @@ class BirthMonthFortuneTeller implements FortuneTellerInterface
 
     public function __construct(CrawlerInterface $crawler, SiteInterface $site)
     {
-        // TODO サイト上での順位の順番はサイトクラスへ
-        $ranking = [2,3,4,5,6,7,8,9,10,11,1,12];
-
         $rows = [];
         $rows = $crawler->get($site);
 
-        $result = array_combine($ranking, $rows);
+        $result = array_combine($site->ranking(), $rows);
 
         // 1位、12位以外は要素が一つ多いので1位、12位に合わせる
         for ($i = 2; $i < 12; $i++) {
@@ -33,20 +31,20 @@ class BirthMonthFortuneTeller implements FortuneTellerInterface
         $birthMonth = '7月';
 
         // $res[0 => 月, 1 => 占い結果, 2 => 色]
+        $rowKeys = $site->rowKeys();
+
         foreach ($result as $rank => $res) {
 
             // 誕生月をセットしたいのでそれ以外は無視
-            if ($birthMonth !== $res[0]) {
+            if ($birthMonth !== $res[$rowKeys['month']]) {
                 continue;
             }
 
             $this->rank = $rank;
-            $this->month = $res[0];
-            $this->comment = $res[1];
-            $this->color = $res[2];
-
-            // TODO 超スッキりす、スッキりす、まあまあスッキりす、ガッカりすを順位から格納
-            $this->label = '';
+            $this->month = $res[$rowKeys['month']];
+            $this->comment = $res[$rowKeys['comment']];
+            $this->color = $res[$rowKeys['color']];
+            $this->label = $site->label($rank);
         }
     }
 
