00b5bdbcdbc03a49abc2787b6923270fd772db6d
f8hash <windboy.fhash@gmail.com>
2020-06-14 13:27:15 +0900

    Update 複雑化させていた層を削除

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
 
-require_once './vendor/autoload.php';
-require_once './sukkirisu.php';
+require_once('vendor/autoload.php');
+require_once('Sukkirisu.php');
+require_once('SukkirisuCrawler.php');
+require_once('SukkirisuSite.php');
+require_once('ConsoleViewer.php');
 
-$sukkirisu = new Sukkirisu;
-$sukkirisu->birthMonthFortuneTelling();
+// webサイトからhtmlを取得
+$teller = new Sukkirisu(new SukkirisuCrawler, new SukkirisuSite);
+
+// 表示
+$viewer = new ConsoleViewer;
+$viewer->show($teller->tell());
--- sukkirisu.php
+++ sukkirisu.php
@@ -1,19 +1,58 @@
 <?php
 
-require_once('ConsoleViewer.php');
-require_once('SukkirisuCrawler.php');
-require_once('SukkirisuSite.php');
-require_once('BirthMonthFortuneTeller.php');
+require_once('FortuneTellerInterface.php');
 
-class Sukkirisu
+// TODO スッキりす用の派生クラスを作成
+class Sukkirisu implements FortuneTellerInterface
 {
-    public function birthMonthFortuneTelling(): void
-    {
-        // webサイトからhtmlを取得
-        $teller = new BirthMonthFortuneTeller(new SukkirisuCrawler, new SukkirisuSite);
+    // 以下の要素を読み取るクラス
+    private $rank;
+    private $month;
+    private $comment;
+    private $color;
+    private $label;
 
-        // 表示
-        $viewer = new ConsoleViewer;
-        $viewer->show($teller->tell());
+    public function __construct(CrawlerInterface $crawler, SiteInterface $site)
+    {
+        $rows = [];
+        $rows = $crawler->get($site);
+
+        $result = array_combine($site->ranking(), $rows);
+
+        // 1位、12位以外は要素が一つ多いので1位、12位に合わせる
+        for ($i = 2; $i < 12; $i++) {
+            array_shift($result[$i]);
+        }
+
+        // TODO 外に定数で定義
+        $birthMonth = '7月';
+
+        // $res[0 => 月, 1 => 占い結果, 2 => 色]
+        $rowKeys = $site->rowKeys();
+
+        foreach ($result as $rank => $res) {
+
+            // 誕生月をセットしたいのでそれ以外は無視
+            if ($birthMonth !== $res[$rowKeys['month']]) {
+                continue;
+            }
+
+            $this->rank = $rank;
+            $this->month = $res[$rowKeys['month']];
+            $this->comment = $res[$rowKeys['comment']];
+            $this->color = $res[$rowKeys['color']];
+            $this->label = $site->label($rank);
+        }
+    }
+
+    public function tell(): array
+    {
+        return [
+            $this->label.'！',
+            $this->month.'生まれは',
+            $this->rank.'位。',
+            $this->comment.'。ラッキーカラーは',
+            $this->color,
+        ];
     }
 }
